39651949afcd4938016bf2b05e8c16312f9148e7
icam <charles.gauthereau@2024.icam.fr>
2023-10-12 08:58:55 +0200

    roulette

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import {Wheel} from "react-custom-roulette";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const data = [
+        { option: '0' },
+        { option: '1' },
+        { option: '2' },
+        { option: '3' },
+        { option: '4' },
+        { option: '5' },
+    ]
+    const [mustSpin, setMustSpin] = useState(false);
+    const [prizeNumber, setPrizeNumber] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleSpinClick = () => {
+        if (!mustSpin) {
+            const newPrizeNumber = Math.floor(Math.random() * data.length);
+            setPrizeNumber(newPrizeNumber);
+            setMustSpin(true);
+        }
+    }
+
+    return (
+        <>
+            <p>Bienvenue dans Randomblast</p>
+            <Wheel
+                mustStartSpinning={mustSpin}
+                prizeNumber={prizeNumber}
+                data={data}
+
+                onStopSpinning={() => {
+                    setMustSpin(false);
+                }}
+            />
+            <button onClick={handleSpinClick}>Lancer la randomBlast</button>
+        </>
+    )
 }
 
 export default App
